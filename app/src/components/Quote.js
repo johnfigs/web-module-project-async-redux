c7dcf202875d8b077e5008571c978f40c717785b
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-08-11 22:21:59 -0700

    completed project setup and moving forward

--- app/src/components/Quote.js
+++ app/src/components/Quote.js
@@ -4,8 +4,8 @@ const Quote = () => {
 
     return (
         <div>
-            <p>That's right! Whatever the tragedy or the blunder is, only the things that actually happened are written in stone!</p>
-            <p>-Donquixote Doflamingo from One Piece</p>
+            <p></p>
+            <p></p>
         </div>
     )
 }
---
+++ app/src/reducers/index.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+    quote: {
+        anime:"One Piece",
+        character: "Donquixote Doflamingo",
+        quote: "That's right! Whatever the tragedy or the blunder is, only the things that actually happened are written in stone!"
+    },
+    isFetching: false,
+    error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+
+}
