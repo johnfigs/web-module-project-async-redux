c2b283b492c645b95cb63af5ed470c8aeb6621cf
Johnathan Figueroa <johnathan.c.figueroa@gmail.com>
2021-08-11 22:05:03 -0700

    Completed app setup and visualized something on app component

---
+++ app/src/components/Quote.js
@@ -0,0 +1,13 @@
+
+
+const Quote = () => {
+
+    return (
+        <div>
+            <p>That's right! Whatever the tragedy or the blunder is, only the things that actually happened are written in stone!</p>
+            <p>-Donquixote Doflamingo from One Piece</p>
+        </div>
+    )
+}
+
+export default Quote;
